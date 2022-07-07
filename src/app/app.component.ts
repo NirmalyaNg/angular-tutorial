9cf4092ae7ce9993b5f1afeede23643ba2a544c0
Nirmalya <nirmalyaganguly1999@gmail.com>
2022-07-07 19:27:48 +0530

    Angular Day 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-introduction';
+  public title: string = 'angular-introduction';
+  public showTitle: boolean = true;
+  public titleColor: string = 'green';
+  public isParagrapahModified: boolean = true;
+  public products = [
+    {
+      id: 'p1',
+      name: 'Samsung Phone',
+      description: 'This is a Samsung Phone',
+    },
+    {
+      id: 'p2',
+      name: 'Micromax Phone',
+      description: 'This is a Micromax Phone',
+    },
+    {
+      id: 'p3',
+      name: 'Apple IPhone',
+      description: 'This is an Apple IPhone',
+    },
+    {
+      id: 'p4',
+      name: 'OnePlus Phone',
+      description: 'This is a OnePlus Phone',
+    },
+  ];
+
+  public showTitleHandler() {
+    console.log('Show Title Button Clicked');
+    this.showTitle = true;
+  }
+
+  public hideTitleHandler() {
+    console.log('Hide Title Button Clicked');
+    this.showTitle = false;
+  }
+
+  public toggleParagraphClass() {
+    this.isParagrapahModified = !this.isParagrapahModified;
+  }
+
+  public getColor() {
+    return 'violet';
+  }
 }
