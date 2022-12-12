612b67a04aed7bc6b05a00f251b5d7393fcb9962
Nirmalya <nirmalyaganguly1999@gmail.com>
2022-12-12 21:03:08 +0530

    Reactive Form Part 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angular-introduction';
+export class AppComponent implements OnInit {
+  signupForm: FormGroup;
+
+  ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+    });
+  }
+
+  handleSubmit() {
+    console.log(this.signupForm);
+  }
 }
