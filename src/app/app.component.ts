fdcdbda81616432670e83ea07e5130a54d63ae9c
Nirmalya <nirmalyaganguly1999@gmail.com>
2022-12-23 00:47:26 +0530

    Day 24

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -10,10 +10,28 @@ export class AppComponent implements OnInit {
   signupForm: FormGroup;
 
   ngOnInit(): void {
+    // Validations:- username (required, min length of 6 characters), email (required, valid email)
     this.signupForm = new FormGroup({
-      username: new FormControl('', Validators.required),
+      username: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
       email: new FormControl('', [Validators.required, Validators.email]),
+      address: new FormGroup({
+        city: new FormControl('Kolkata', Validators.required),
+        state: new FormControl('West Bengal', Validators.required),
+      }),
+      skills: new FormArray([]),
     });
+  }
+
+  onAddSkill() {
+    const control = new FormControl('', Validators.required);
+    (this.signupForm.get('skills') as FormArray).push(control);
+  }
+
+  getControls() {
+    return (this.signupForm.get('skills') as FormArray).controls;
   }
 
   handleSubmit() {
