f49634787a7fd9196144dc9249de6ec0e75bdbf9
Nirmalya <nirmalyaganguly1999@gmail.com>
2022-12-24 17:34:09 +0530

    Day 24 Part 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,15 +8,20 @@ import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 })
 export class AppComponent implements OnInit {
   signupForm: FormGroup;
+  restrictedUsernames = ['ADMIN', 'admin'];
 
   ngOnInit(): void {
     // Validations:- username (required, min length of 6 characters), email (required, valid email)
     this.signupForm = new FormGroup({
       username: new FormControl('', [
         Validators.required,
-        Validators.minLength(6),
+        Validators.minLength(5),
+        this.isRestricted.bind(this),
       ]),
-      email: new FormControl('', [Validators.required, Validators.email]),
+      email: new FormControl('test@test.com', [
+        Validators.required,
+        Validators.email,
+      ]),
       address: new FormGroup({
         city: new FormControl('Kolkata', Validators.required),
         state: new FormControl('West Bengal', Validators.required),
@@ -41,11 +46,28 @@ export class AppComponent implements OnInit {
     });
   }
 
+  onSuggestUsername() {
+    // this.signupForm.setValue({
+    //   username: 'TestUser1234',
+    //   email: this.signupForm.value.email,
+    //   address: {
+    //     city: this.signupForm.value.address.city,
+    //     state: this.signupForm.value.address.state,
+    //   },
+    //   skills: this.signupForm.value.skills,
+    // });
+
+    this.signupForm.patchValue({
+      username: 'TestUser123456',
+    });
+  }
+
   onAddSkill() {
     const control = new FormControl('', Validators.required);
     (this.signupForm.get('skills') as FormArray).push(control);
   }
 
+  // The number of inputs that will be displayed depends on the number of controls inside skills which is a FormArray
   getControls() {
     return (this.signupForm.get('skills') as FormArray).controls;
   }
@@ -56,5 +78,17 @@ export class AppComponent implements OnInit {
 
   handleSubmit() {
     console.log(this.signupForm);
+  }
+
+  isRestricted(control: FormControl): { [key: string]: boolean } {
+    // If you return an object the control will be invalid else if you return null the control will be valid
+    const value = control.value;
+    if (this.restrictedUsernames.includes(value)) {
+      return {
+        restricted: true,
+      };
+    } else {
+      return null;
+    }
   }
 }
